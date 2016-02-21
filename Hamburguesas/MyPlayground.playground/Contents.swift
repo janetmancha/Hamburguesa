1cf3bf60901541181f922fb52f66bd26dfbec04d
Janet Mancha <janet.mancha@gmail.com>
2016-02-21 12:06:58 +0100

    inclusion playground

---
+++ Hamburguesas/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//playground para probar antes de llevarlo al codigo final
+
+
+
+
+
+
+UIColor(red: 300/255.0, green: 400/255.0, blue: 40/255.0,alpha:1)
+UIColor(red: 500/255.0, green: 100/255.0, blue: 200/255.0,alpha:1)
+UIColor(red: 55/255.0, green: 200/255.0, blue: 2/255.0,alpha:1)
+UIColor(red: 1/255.0, green: 70/255.0, blue: 300/255.0,alpha:1)
+UIColor(red: 300/255.0, green: 2/255.0, blue: 1/255.0,alpha:1)
+UIColor(red: 600/255.0, green: 300/255.0, blue: 300/255.0,alpha:1)
+UIColor(red: 150/255.0, green: 600/255.0, blue: 300/255.0,alpha:1)
+UIColor(red: 100/255.0, green: 1/255.0, blue: 300/255.0,alpha:1)
